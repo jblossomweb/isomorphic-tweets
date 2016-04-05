a00b7894f3106ce8586a663e6eb624d9baa5a25c
John Blossom <jblossom.web@gmail.com>
2016-04-05 14:26:43 -0400

    implement .env in favor of hardcoded config, mongoose bugfix

--- config.js
+++ config.js
@@ -1,8 +1,18 @@
+var fs = require('fs')
+var env = require('node-env-file')
+
+if (fs.existsSync('.env')) {
+  env('.env')
+}
+
 module.exports = {
+  mongo: {
+    connection: process.env.MONGO_CONNECTION || 'mongodb://localhost/react-tweets'
+  },
   twitter: {
-    consumer_key: 'MY_CONSUMER_KEY',
-    consumer_secret: 'MY_CONSUMER_SECRET',
-    access_token_key: 'MY_ACCESS_TOKEN_KEY',
-    access_token_secret: 'MY_ACCESS_TOKEN_SECRET'
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
   }
 }
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set('view engine', 'handlebars');
 app.disable('etag');
 
 // Connect to our mongo database
-mongoose.connect('mongodb://localhost/react-tweets');
+mongoose.connect(config.mongo.connection);
 
 // Create a new ntwitter instance
 var twit = new twitter(config.twitter);
